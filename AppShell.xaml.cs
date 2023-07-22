793e72b62875caab2463fe64e8fc50375cd0d919
hathu <hathu@HATHUCHUNG>
2023-07-23 01:46:29 +0700

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SeleniumTikTok;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SeleniumTikTok;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
